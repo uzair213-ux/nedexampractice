2bb6fd114fb76ab3d543443929e0f68ca5d4b9c8
syeduzairtirmizi <tirmizisyeduzair@gmail.com>
2025-07-01 16:01:31 +0500

    Manually updating favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import Header from '@/components/Header';
 export const metadata: Metadata = {
   title: 'NED Exam',
   description: 'MCQ Test Portal',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎓</text></svg>',
+  },
 };
 
 export default function RootLayout({
